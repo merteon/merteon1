8879f8943792f141c4144db72b74d38f6da9f3e5
merteon <91878252+merteon@users.noreply.github.com>
2021-10-21 14:33:09 +0900

    Update upbitrising2.py

--- upbitrising2.py
+++ upbitrising2.py
@@ -109,7 +109,7 @@ def 총매수금액():
 
 while True:
     time.sleep(1)
-    bot.sendMessage(chat_id=chat_id, text="코인 급등주 자동 매가 {0}분 동안 작동 중입니다.".format(index))
+    bot.sendMessage(chat_id=chat_id, text="코인 급등주 자동 매도가 {0}분 동안 작동 중입니다.".format(index))
     index += 10
     for ticker in tickers:
         if 잔고(ticker) !=0:
