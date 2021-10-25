a999765f75ee5bbd528423c24bcf087b67fe5e48
merteon <91878252+merteon@users.noreply.github.com>
2021-10-25 13:49:47 +0900

    Update upbitrising0.py

--- upbitrising0.py
+++ upbitrising0.py
@@ -3,7 +3,7 @@ import pyupbit
 import time
 import telegram
 
-bot = telegram.Bot(token='df')
+bot = telegram.Bot(token='2002300988:AAHmktc9k6NGM_MS-wQKK2FiM3ys8emrB9Q')
 chat_id = 2086229730
 
 access = "tmobuBWWxvYxV9B2oW8ewuv0VsRZXEj8NIApk1Qn" 
